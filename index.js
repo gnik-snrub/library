dbc94f5c9d992f214dd7b525daa003538024c45f
Josiah Morris <josiahbmorris@hotmail.com>
2021-01-13 16:35:54 +1100

    Refactored book rendering loop. Moved book panel creation to separate function within Book object.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 let myLibrary = [];
 
 function Book(title, author, year, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.year = year.toString();
-  this.pages = pages + ' pg';
-  this.read = read;
+    this.title = title;
+    this.author = author;
+    this.year = year.toString();
+    this.pages = pages + ' pg';
+    this.read = read;
+
+    this.addBook = function() {
+        const bookPanel = document.createElement('div');
+        bookPanel.classList.add('book');
+
+        const title = document.createElement('h2');
+        title.classList.add('title')
+        title.textContent = this.title;
+        bookPanel.appendChild(title);
+
+        const author = document.createElement('h3');
+        author.classList.add('author')
+        author.textContent = this.author;
+        bookPanel.appendChild(author);
+
+        const year = document.createElement('div');
+        year.classList.add('year')
+        year.textContent = this.year;
+        bookPanel.appendChild(year);
+
+        const pages = document.createElement('div');
+        pages.classList.add('pages')
+        pages.textContent = this.pages;
+        bookPanel.appendChild(pages);
+
+        const read = document.createElement('input');
+        read.classList.add('read')
+        read.setAttribute('type', 'checkbox');
+        if (this.read) {
+            read.checked = true;
+        } else {
+            read.checked = false;
+        }
+        bookPanel.appendChild(read);
+        return bookPanel
+    }
 }
 
 let hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 1937, 310, false);
@@ -26,42 +62,8 @@ function addBooks() {
     while (container.lastElementChild) {
         container.removeChild(container.lastElementChild);
     }
-
-    for (bookId in myLibrary) {
-        const bookPanel = document.createElement('div');
-        bookPanel.classList.add('book');
-
-        const title = document.createElement('h2');
-        title.classList.add('title')
-        title.textContent = myLibrary[bookId].title;
-        bookPanel.appendChild(title);
-
-        const author = document.createElement('h3');
-        author.classList.add('author')
-        author.textContent = myLibrary[bookId].author;
-        bookPanel.appendChild(author);
-
-        const year = document.createElement('div');
-        year.classList.add('year')
-        year.textContent = myLibrary[bookId].year;
-        bookPanel.appendChild(year);
-
-        const pages = document.createElement('div');
-        pages.classList.add('pages')
-        pages.textContent = myLibrary[bookId].pages;
-        bookPanel.appendChild(pages);
-
-        const read = document.createElement('input');
-        read.classList.add('read')
-        read.setAttribute('type', 'checkbox');
-        if (myLibrary[bookId].read) {
-            read.checked = true;
-        } else {
-            read.checked = false;
-        }
-        bookPanel.appendChild(read);
-
-        container.appendChild(bookPanel);
+    for (book in myLibrary) {
+        container.appendChild(myLibrary[book].addBook());
     }
 }
 
